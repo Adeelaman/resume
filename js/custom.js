727a8187293c7760462d2f0e007486dde4bda328
Adeelaman <aa.qurashi@yahoo.com>
2022-03-15 04:29:17 +0500

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -6,12 +6,12 @@
  */
 $('.download_resume').on('click', function (e) {
     e.preventDefault();  //stop the browser from following
-    var link = document.createElement('a');
-    link.href = "/files/adeel_s_resume.pdf";
-    link.download = "Adeel_s_resume.pdf";
-    link.click();
-    link.remove();
-    // window.location.href = '/files/adeel_s_resume.pdf';
+    // var link = document.createElement('a');
+    // link.href = "/files/adeel_s_resume.pdf";
+    // link.download = "Adeel_s_resume.pdf";
+    // link.click();
+    // link.remove();
+    window.location.href = '/files/adeel_s_resume.pdf';
     return false;
 
     // let req = new XMLHttpRequest();
